081f7d3ba4580a244e72ddc12dc49f0b882bbe25
li xie <huge@sc7y4.onmicrosoft.com>
2023-08-10 22:04:47 +0800

    add moremisc

---
+++ DS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+struct type_name {
+	int member_name1;
+	char member_name2;
+} object_names;
+int main{
+	type_name object_names1;
+cout << " member_name: " << object_names.member_name1 << endl;
+
+
+
+	return 0;
+}
